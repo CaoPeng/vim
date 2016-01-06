2eb8078a6363c4096f9f2491dd63b5ffe62b163a
peng.cao <a3825aa@gmail.com>
2016-01-06 11:19:21 +0800

    upgrade script for crate ctags/cscope

--- tools/cctags.sh
+++ tools/cctags.sh
@@ -13,15 +13,20 @@ fi
 if [ -d ${SRC_PATH}/frameworks -a -d ${SRC_PATH}/packages -a -d ${SRC_PATH}/hardware -a -d ${SRC_PATH}/system -a -d ${SRC_PATH}/libcore ];
 then
     echo "Found framework packages hardware and system directory!!"
-    find -L ${SRC_PATH}/frameworks -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.c" -o -name "*.java"|grep -vi '\.git' >${OUT_PATH}/names.file
+    find -L ${SRC_PATH}/frameworks -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name \
+        "*.c" -o -name "*.java"|grep -vi '\.git' >${OUT_PATH}/names.file
     echo "framework scann finish!"
-    find -L ${SRC_PATH}/packages -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
+    find -L ${SRC_PATH}/packages -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name \
+        "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
     echo "packages scann finish!!"
-    find -L ${SRC_PATH}/hardware -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
+    find -L ${SRC_PATH}/hardware -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name \
+        "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
     echo "hardware scann finish!!"
-    find -L ${SRC_PATH}/system -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
+    find -L ${SRC_PATH}/system -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name \
+        "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
 
-    find -L ${SRC_PATH}/libcore -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
+    find -L ${SRC_PATH}/libcore -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name \
+        "*.c" -o -name "*.java"|grep -vi '\.git' >>${OUT_PATH}/names.file
 
     echo "sytstem scann finish!!"
 else
@@ -29,8 +34,7 @@ else
 fi
 
 echo "For create ctags databases.."
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -L ${OUT_PATH}/names.file -f ${OUT_PATH}/tags
-mv names.file cscope.files
+ctags -R --c++-kinds=+p+l+x+c+d+e+f+g+m+n+s+t+u+v --fields=+liaS --extra=+q -L ${OUT_PATH}/names.file -f ${OUT_PATH}/tags
 echo "for create cscope databases.."
-cscope -Rbq -i cscope.files
+cscope -Rbq -i names.file
 echo "DONE!!"
